1141c5213a30e9fcca2492ac792590066763e82d
ChaeHo95 <chlcogh11@gmail.com>
2024-11-03 01:26:22 +0900

    test: 환경 변수 설정 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-
+const product = process.env.REPO_NAME || '';
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
@@ -7,11 +7,11 @@ export default function Home() {
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
         <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
           <li className="mb-2">
-            Get started by editing{' '}
+            Get started by editing
             <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
               src/app/page.tsx
             </code>
-            .
+            . product:{product}
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
