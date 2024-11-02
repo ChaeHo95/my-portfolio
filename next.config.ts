e5c299ce069e11cc331473f0b63d9b6b1d189392
ChaeHo95 <chlcogh11@gmail.com>
2024-11-03 03:31:45 +0900

    test: 환경 변수 설정 테스트

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from 'next';
 
-const product = process.env.NEXT_PUBLIC_REPO_NAME || '';
-
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: product,
   images: {
     unoptimized: true,
   },
