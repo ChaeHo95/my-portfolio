d298c6d15d3a3efd2e1889e2d44ec7e420a3a0b0
ChaeHo95 <chlcogh11@gmail.com>
2024-11-03 01:01:10 +0900

    config: 환경 변수 설정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from 'next';
 
+const product = process.env.REPO_NAME;
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  assetPrefix: product,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
