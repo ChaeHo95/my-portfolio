9ffd7169374e9a31047a3f029b1999403419e46b
ChaeHo95 <chlcogh11@gmail.com>
2024-11-03 13:04:29 +0900

    feat: 로딩 추가 및 플로팅 버튼 연동

--- src/components/ClientThemeProvider.tsx
+++ src/components/ClientThemeProvider.tsx
@@ -1,20 +1,55 @@
 'use client';
-
-import { CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material';
-import { ReactNode, useMemo } from 'react';
+import { Box, CssBaseline, LinearProgress, ThemeProvider, Typography, useMediaQuery } from '@mui/material';
+import { ReactNode, useEffect, useMemo, useState } from 'react';
 import { createAppTheme } from '@/assets/styles/theme';
+import DarkModeToggle from '@/components/DarkModeToggle';
+
+const ClientThemeProvider = ({ children }: ClientThemeProviderProps) => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    setIsDarkMode(prefersDarkMode);
+  }, [prefersDarkMode]);
+
+  const theme = useMemo(() => createAppTheme(isDarkMode ? 'dark' : 'light'), [isDarkMode]);
+
+  if (loading) {
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        minHeight="100vh"
+        bgcolor="background.default"
+        className="rounded-lg border p-4"
+      >
+        <Typography variant="h6" mb={2}>
+          Loading...
+        </Typography>
+        <LinearProgress style={{ width: '80%' }} />
+      </Box>
+    );
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+      <DarkModeToggle isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+    </ThemeProvider>
+  );
+};
 
 type ClientThemeProviderProps = {
   children: ReactNode;
 };
 
-export default function ClientThemeProvider({ children }: ClientThemeProviderProps) {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const theme = useMemo(() => createAppTheme(prefersDarkMode ? 'dark' : 'light'), [prefersDarkMode]);
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
-  );
-}
+export default ClientThemeProvider;
